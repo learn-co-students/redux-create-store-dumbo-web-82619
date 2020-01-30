fca46009ec7df4db5af48a95b3102f3261272177
Lilaro <larochelle.lisa@gmail.com>
2020-01-30 15:37:40 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,5 +1,6 @@
 function createStore(reducer) {
-  let state;
+  let state; //declaring state as a variable so that we may pass it as an argument
+             //with a default value(so that we can render it at 0 on '@@INIT')
 
   function dispatch(action){
     state = reducer(state, action);
@@ -13,7 +14,8 @@ function createStore(reducer) {
   return {
     dispatch,
     getState
-  }
+  }  //gives us access to these functions as methods on createStore 
+     //(which we assign to the 'store' variabe below)
 }
 
 function reducer(state = { count: 0 }, action) {
@@ -33,7 +35,7 @@ function render() {
 
 let store = createStore(reducer)
 
-store.dispatch({type: '@@INIT'})
+store.dispatch({type: '@@INIT'}); //call dispatch right away in order to render default state
 
 let button = document.getElementById('button');
 
